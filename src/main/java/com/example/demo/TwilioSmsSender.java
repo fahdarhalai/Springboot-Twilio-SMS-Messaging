e3b39facee78b0f33bcddd8f221cdd11a0a3250b
Fahd Arhalai <fahd.arh@gmail.com>
2020-04-11 23:49:34 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/TwilioSmsSender.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
+import com.twilio.type.PhoneNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service("twilio")
+public class TwilioSmsSender implements SmsSender {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwilioSmsSender.class);
+    private final TwilioConfiguration twilioConfiguration;
+
+    public TwilioSmsSender(TwilioConfiguration twilioConfiguration) {
+        this.twilioConfiguration = twilioConfiguration;
+    }
+
+    @Override
+    public void sendSms(SmsRequest smsRequest) {
+        if(isPhoneNumberValid(smsRequest.getPhoneNumber())){
+            PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
+            PhoneNumber from = new PhoneNumber(twilioConfiguration.getTrialNumber());
+            MessageCreator creator = Message.creator(to, from, smsRequest.getMessage());
+            creator.create();
+            LOGGER.info("Send sms {}"+smsRequest);
+        }else {
+            throw new IllegalArgumentException("Phone number ["+smsRequest.getPhoneNumber()+"] is not a valid number");
+        }
+
+    }
+
+    private boolean isPhoneNumberValid(String phoneNumber) {
+        //
+        return true;
+    }
+}
